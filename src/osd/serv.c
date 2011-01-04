42dd9875e974d038ec6c7aef5c84d5b7f4548116
idning <idning@gmail.com>
2011-01-04 17:25:05 +0000

    abc

--- src/osd/serv.c
+++ src/osd/serv.c
@@ -1,6 +1,26 @@
 #include "network.h"
 
 int main(){
-    int ss = server_socket("127.0.0.1", "9991");
+    server();
 
 }
+
+int server(){
+    char buffer[1024];
+    int ss = server_socket("127.0.0.1", "9991");
+    if (ss < 0){
+        logging(LOG_ERROR, "socket error!");
+        return -1;
+    }
+    int ns = tcptoaccept(ss, 100000);
+    if (ns < 0){
+        logging(LOG_ERROR, "accept error!");
+        return -1;
+    }
+    tcpnonblock(ns);
+    tcpnodelay(ns);
+    int n = tcptoread(ns, buffer, 10, 3000);
+    printf("read ok %d !!\n", n);
+    printf("read data: \n%s\n", buffer);
+}
+
