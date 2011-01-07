7e2a9bd1153febf6dc0df5350ea25a8e96c68986
idning <idning@gmail.com>
2011-01-07 05:55:29 +0000

    现有代码备份，下一次是生成的代码

--- src/common/protocol.h
+++ src/common/protocol.h
@@ -2,52 +2,48 @@
 
 /*master<-client request*/
 typedef struct mc_mkdir_request{
-    int msgid;
-    int version;
-    int operation;
-    int msglength;
+    uint32_t msgid; /*abc*/
+    uint32_t version;
+    uint32_t operation;
+    uint32_t msglength;
 
-    int parent;
-    int namelenght;
-    uint8_t * name;
+    uint32_t parent;
+    uint32_t namelength;
+    uint8_t* name;
     uint32_t mode;
-    int uid;
-    int gid;
+    uint32_t uid;
+    uint32_t gid;
 
-    /*--*/
-    void (*pack)  (struct mc_mkdir_request * req, uint8_t * data, int len) ;
-    void (*unpack)(struct mc_mkdir_request * req, uint8_t * data, int len);
+    /*-- for protocol_gen*/
+    void (*pack)  (struct mc_mkdir_request * req, uint8_t * data, uint32_t len) ;
+    void (*unpack)(struct mc_mkdir_request * req, uint8_t * data, uint32_t len);
 }mc_mkdir_request;
 
 
 /*osd<-client request*/
 typedef struct oc_request{
-    int msgid;
-    int version;
-    int operation;
-    int msglength;
-    uint8_t * data;
+    uint32_t msgid;
+    uint32_t version;
+    uint32_t operation;
+    uint32_t msglength;
+    uint8_t* data;
     /*--*/
-    void (*pack) (struct oc_request * req, uint8_t * data, int len) ;
-    void (*unpack)(struct oc_request * req, uint8_t * data, int len);
+    void (*pack) (struct oc_request * req, uint8_t * data, uint32_t len) ;
+    void (*unpack)(struct oc_request * req, uint8_t * data, uint32_t len);
 }oc_request;
 
 /*osd<-client response*/
 typedef struct oc_response{
-    int msgid;
-    int version;
-    int operation;
-    int msglength;
+    uint32_t msgid;
+    uint32_t version;
+    uint32_t operation;
+    uint32_t msglength;
 }oc_response;
 
+void oc_request_pack(struct oc_request * req, uint8_t * data, uint32_t len);
+void oc_request_unpack(struct oc_request * req, uint8_t * data, uint32_t len);
 
-
-
-
-void oc_request_pack(struct oc_request * req, uint8_t * data, int len);
-void oc_request_unpack(struct oc_request * req, uint8_t * data, int len);
-
-void mc_mkdir_request_pack(struct mc_mkdir_request * req, uint8_t * data, int len);
-void mc_mkdir_request_unpack(struct mc_mkdir_request * req, const uint8_t * data, int len);
+void mc_mkdir_request_pack(struct mc_mkdir_request * req, uint8_t * data, uint32_t len);
+void mc_mkdir_request_unpack(struct mc_mkdir_request * req, const uint8_t * data, uint32_t len);
 
 
