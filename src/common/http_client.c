9fea135975a014622cf061904f879960823d4ef4
idning <idning@gmail.com>
2011-02-19 18:16:58 +0000

    http client add post

--- src/common/http_client.c
+++ src/common/http_client.c
@@ -1,12 +1,24 @@
 #include "http_client.h"
+#include "network.h"
+
 /*#include <event.h>*/
 /*#include <evhttp.h>*/
+
+
+static struct evbuffer * http_req(char * ip, int port, char * verb, char * path, char * data);
+
+struct evbuffer * http_get(char * ip, int port, char * path){
+    return http_req(ip, port, "GET", path, "");
+}
+struct evbuffer * http_post(char * ip, int port, char * path, char * data){
+    return http_req(ip, port, "POST", path, data);
+}
 
 /*
  *
     should use evbuffer_free(evb); to free this evbuffer
  * */
-struct evbuffer * http_get(char * ip, int port, char * path){
+static struct evbuffer * http_req(char * ip, int port, char * verb, char * path, char * data){
 
     char port_str[6];
     sprintf(port_str, "%d", port);
@@ -15,7 +27,7 @@ struct evbuffer * http_get(char * ip, int port, char * path){
     char * request_buffer = alloca(100 + strlen(path)); // in stack do not need free
     char response_buffer[1024*8];
 
-    sprintf(request_buffer, "GET %s HTTP/1.0\r\n\r\n", path);
+    sprintf(request_buffer, "%s %s HTTP/1.0\r\n\r\n%s", verb, path, data);
     //sprintf(request_buffer, "GET %s HTTP/1.0\r\nUser-Agent: curl/7.19.7 (i486-pc-linux-gnu) libcurl/7.19.7 OpenSSL/0.9.8k zlib/1.2.3.3 libidn/1.15\r\n\r\n", path);
     printf(request_buffer);
 
