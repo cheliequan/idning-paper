2047ede267ca073947d33e49bebdcb1be9b4b4f5
idning@gmail.com <idning@gmail.com@0b1229a6-edc7-a475-7139-65632ce9cc64>
2011-04-13 10:22:56 +0000

    论文里面的很多数据都是用这个版本泡出来的.

---
+++ trunk/docs/bench/0414_mds_test_home_3mds/analysis.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+#coding: utf8
+
+import re,glob
+machines = ['10000', '10001', '10002']
+
+def parse_file(path):
+    data = file(path).read()
+    cluster_maps = data.split('\n------------\n')
+    for c in cluster_maps:
+        #print c
+        kv = {'10000': '', '10001': '', '10002': ''}
+        for m in re.findall(r'\((\d*)\) LOAD: (\d*)', c, re.DOTALL):
+            kv[m[0]] = m[1];
+        print '%s\t%s\t%s'%( kv['10000'], kv['10001'], kv['10002'])
+
+
+
+
+parse_file('log/cmgr.cfg.log')
+    
